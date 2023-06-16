651b983e09c6366ea3773b3d49c69d001cc60b9c
Tejaswini Hirudkar <121488475+TejaswiniHirudkar@users.noreply.github.com>
2023-06-16 21:11:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
-  
+  var tabel=document.getElementById('sampleTable');
+	var newRow=tabel.insertRow(0);
+	var cell1 = newRow.insertCell(0);
+  var cell2 = newRow.insertCell(1);
+	/ Set the cell values
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
   
 }
